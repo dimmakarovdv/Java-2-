b5b8239e566e78a80e7d2d0b8ab86c23b76d8c58
Neto test <dimmakarovdv89@mail.ru>
2025-05-22 19:51:53 +0500

    file modification

--- Java 2 exercise/src/Main.java
+++ Java 2 exercise/src/Main.java
@@ -1,19 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        int x = 100;    // начальная сумма счета
-        int y = 1100;   // сумма пополнения счета
+        int check = 100;    // начальная сумма счета
+        int replenishment = 1100;   // сумма пополнения счета
         int bonus;
-        if (y > 1000) {
-            bonus = (y / 100);  // Бонус начисляется как 1% от суммы пополнения сверх 1000
+        if (replenishment > 1000) {
+            bonus = (replenishment / 100);  // Бонус начисляется как 1% от суммы пополнения сверх 1000
         } else {
             bonus = 0; // Бонус равен 0 если сумма пополнения менее 1000
         }
-        int z = x + y + bonus;  // Итоговая сумма должна включать бонусы
+        int z = check + replenishment + bonus;  // Итоговая сумма должна включать бонусы
 
         System.out.println("Итоговая сумма на счете Клиента после пополнения с учетом бонуса = " + z);
         System.out.println("В том числе сумма начисленных бонусов за пополнение счета = " + bonus);
-}
+    }
 }
